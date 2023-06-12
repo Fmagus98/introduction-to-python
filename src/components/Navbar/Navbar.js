60f51308e53ea438d2606471d682d04feed796fe
federico martearena <93057719+Fmagus98@users.noreply.github.com>
2023-06-12 15:25:46 -0300

    fix navbar-mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,52 +3,52 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   const [backgroundColor, setBackgroundColor] = useState('');
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollPosition = window.scrollY;
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
       const eightyPercentHeight = window.innerHeight * 0.8;
-  
-        if (scrollPosition >= eightyPercentHeight) {
-        setBackgroundColor('#010483'); 
-        } else {
-        setBackgroundColor(''); 
-        }
-      };
 
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
-
-    const handleMobileNavToggle = () => {
-      setIsMobileNavOpen(!isMobileNavOpen);
+      if (scrollPosition >= eightyPercentHeight) {
+        setBackgroundColor('#010483');
+      } else {
+        setBackgroundColor('');
+      }
     };
 
-      return (
-      <header id="header" className="fixed-top d-flex align-items-center header-transparent" style={{ background: backgroundColor }}>
-        <div className="container d-flex align-items-center justify-content-between">
-          <div className="logo">
-            <h1><a href="/"><span>Fablab - python</span></a></h1>
-            <a href="/"><img src="assets/img/logo.png" alt="" className="img-fluid" /></a>
-          </div>
-          <nav id="navbar" className={isMobileNavOpen ? 'navbar navbar-mobile h-auto' : 'navbar'}>
-            <ul className={isMobileNavOpen ? 'h-auto' :''}>
-              <li onClick={handleMobileNavToggle} ><Link to="/" style={isMobileNavOpen ? {color: "##010483"} : null } className={isMobileNavOpen ? 'nav-link scrollto active' :''}>Inicio</Link></li>
-              <li onClick={handleMobileNavToggle} ><Link to="/classes" style={isMobileNavOpen ? {color: "##010483"} : null } className={isMobileNavOpen ? 'nav-link scrollto active' :''}>Clases</Link></li>
-              <li onClick={handleMobileNavToggle} ><Link to="/exercises" style={isMobileNavOpen ? {color: "##010483"} : null } className={isMobileNavOpen ? 'nav-link scrollto active' :''}>Ejercicios</Link></li>
-              <li onClick={handleMobileNavToggle} ><Link to="/" style={isMobileNavOpen ? {color: "##010483"} : null } className={isMobileNavOpen ? 'nav-link scrollto active' :''}>Projectos</Link></li>
-              <li onClick={handleMobileNavToggle} ><Link to="/contact" style={isMobileNavOpen ? {color: "##010483"} : null } className={isMobileNavOpen ? 'nav-link scrollto active' :''}>Contacto</Link></li>
-            </ul>
-            <i className={`mobile-nav-toggle ${isMobileNavOpen ? 'bi-x' : 'bi-list'}`}
-          onClick={handleMobileNavToggle}></i>
-          </nav>
-        </div>
-      </header>
+    window.addEventListener('scroll', handleScroll);
 
-      )
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  const handleMobileNavToggle = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  return (
+    <header id="header" className="fixed-top d-flex align-items-center header-transparent" style={{ background: backgroundColor }}>
+      <div className="container d-flex align-items-center justify-content-between">
+        <div className="logo">
+          <h1><a href="/"><span>Fablab - python</span></a></h1>
+          <a href="/"><img src="assets/img/logo.png" alt="" className="img-fluid" /></a>
+        </div>
+        <nav id="navbar" className={isMobileNavOpen ? 'navbar navbar-mobile h-auto' : 'navbar'}>
+          <ul className={isMobileNavOpen ? 'h-auto' : ''}>
+            <li><Link to="/" style={isMobileNavOpen ? { color: "##010483" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Inicio</Link></li>
+            <li><Link to="/classes" style={isMobileNavOpen ? { color: "##010483" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Clases</Link></li>
+            <li><Link to="/exercises" style={isMobileNavOpen ? { color: "##010483" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Ejercicios</Link></li>
+            <li><Link to="/" style={isMobileNavOpen ? { color: "##010483" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Projectos</Link></li>
+            <li><Link to="/contact" style={isMobileNavOpen ? { color: "##010483" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Contacto</Link></li>
+          </ul>
+          {isMobileNavOpen ? null : <i className={`mobile-nav-toggle ${isMobileNavOpen ? 'bi-x' : 'bi-list'}`}
+            onClick={handleMobileNavToggle}></i>}
+        </nav>
+      </div>
+    </header>
+
+  )
 }
 
-      export default Navbar
+export default Navbar
