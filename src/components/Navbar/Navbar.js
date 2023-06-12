3864bd3d945d0e0c93dbe7ffa31d2b61699cb947
federico martearena <93057719+Fmagus98@users.noreply.github.com>
2023-06-12 15:55:25 -0300

    fix navbar-mobile4

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,16 +7,13 @@ const Navbar = () => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const eightyPercentHeight = window.innerHeight * 0.8;
-
       if (scrollPosition >= eightyPercentHeight) {
         setBackgroundColor('#010483');
       } else {
         setBackgroundColor('');
       }
     };
-
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -34,8 +31,8 @@ const Navbar = () => {
           <h1><a href="/"><span>Fablab - python</span></a></h1>
           <a href="/"><img src="assets/img/logo.png" alt="" className="img-fluid" /></a>
         </div>
-        <nav id="navbar" className={isMobileNavOpen ? 'navbar navbar-mobile h-auto' : 'navbar'}>
-          <ul className={isMobileNavOpen ? 'h-50' : ''}>
+        <nav id="navbar" onClick={isMobileNavOpen ? handleMobileNavToggle:null} className={isMobileNavOpen ? 'navbar navbar-mobile h-auto' : 'navbar'}>
+          <ul className={isMobileNavOpen ? 'h-50 ml-6 mr-6' : ''}>
             <li><Link to="/" style={isMobileNavOpen ? { color: "##010483", fontSize: "1.4rem" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Inicio</Link></li>
             <li><Link to="/classes" style={isMobileNavOpen ? { color: "##010483", fontSize: "1.4rem" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Clases</Link></li>
             <li><Link to="/exercises" style={isMobileNavOpen ? { color: "##010483", fontSize: "1.4rem" } : null} className={isMobileNavOpen ? 'nav-link scrollto active' : ''}>Ejercicios</Link></li>
@@ -47,7 +44,6 @@ const Navbar = () => {
         </nav>
       </div>
     </header>
-
   )
 }
 
