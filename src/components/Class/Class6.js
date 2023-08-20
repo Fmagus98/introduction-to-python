54d1c59b30f2e873af59ac6a75c1e1625161917b
federico martearena <93057719+Fmagus98@users.noreply.github.com>
2023-08-20 18:15:33 -0300

    Update Class6.js

--- src/components/Class/Class6.js
+++ src/components/Class/Class6.js
@@ -195,13 +195,13 @@ const Class6 = () => {
                   </div>
                 </div>
                 <div data-aos="fade-right" className="accordion-item mt-4 rounded-5" style={{ background: "#010483" }}>
-                  <button className="accordion-button collapsed bg-transparent" type="button">
-                    <Link to="/pythonClassFile/class6.py"
+                 <Link to="/pythonClassFile/class6.py"
                       target={"_blank"}
                       download={"class6.py"}>
+                  <button className="accordion-button collapsed bg-transparent" type="button">
                       <h2 data-aos="fade-right" className="text-light">Descarga el archivo de la clase 6</h2>
-                    </Link>
                   </button>
+                 </Link>
                 </div>
               </div>
             </div>
