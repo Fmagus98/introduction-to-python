65858f893f3cb221ad44f8bba8845c9e513f8517
federico martearena <93057719+Fmagus98@users.noreply.github.com>
2024-03-24 16:20:14 -0300

    integration of state copied in CopyButton component

--- src/components/CopyButton/CopyButton.js
+++ src/components/CopyButton/CopyButton.js
@@ -1,7 +1,32 @@
-export const CopyButton = ({code}) => {
+import React, { useState } from 'react';
+
+export const CopyButton = ({ code }) => {
+
+    const [copied, setCopied] = useState(false);
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(code);
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false); // Cambiar de nuevo a "Copiar" después de 2 segundos
+        }, 1000);
+    };
+
     return (
         <div className="w-75 mx-auto d-flex p-2 flex-row-reverse justify-content-between">
-            <span><button className="btn btn-sm text-light" style={{ backgroundColor: "#234A91" }} onClick={() => navigator.clipboard.writeText(code)}><i className="bi bi-clipboard" /> Copiar</button></span>
+            <span>
+                <button
+                    className="btn btn-sm text-light"
+                    style={{ backgroundColor: "#234A91" }}
+                    onClick={copyToClipboard}
+                    disabled={copied} // Deshabilitar el botón cuando se ha copiado
+                >
+                    <i className="bi bi-clipboard" /> {copied ? 'Copiado ✓' : 'Copiar'}
+                </button>
+            </span>
         </div>
-    )
-}
+    );
+};
+
+export default CopyButton;
