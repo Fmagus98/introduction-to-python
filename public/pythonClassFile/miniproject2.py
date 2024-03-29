09c82caa564fcfe43c38d0df3796e6141af6bfca
federico martearena <93057719+Fmagus98@users.noreply.github.com>
2024-03-29 09:18:58 -0300

    added code in miniproject2

--- public/pythonClassFile/miniproject2.py
+++ public/pythonClassFile/miniproject2.py
@@ -3,14 +3,17 @@ import random
 numero_aleatorio = random.randint(0,20)
 intentos = 5
 while True:
-    numero = int(input("Ingresa un numero entre 0 y 20: "))
-    if numero == numero_aleatorio and intentos > 0:
-        print("Ganaste")
+    if intentos == 0:
+        print("Perdiste, te quedaste sin intentos!")
         break
-    elif numero > numero_aleatorio and intentos > 0:
-        print("Mas bajo")
-    elif numero < numero_aleatorio and intentos > 0:
-        print("Mas alto")
-    elif intentos == 0:
-        print("Perdiste")
-        break
+    else:
+        numero = int(input("Ingresa un numero entre 0 y 20: "))
+        if numero == numero_aleatorio and intentos > 0:
+            print("Ganaste!")
+            break
+        elif numero > numero_aleatorio and intentos > 0:
+            intentos -= 1
+            print("Mas bajo")
+        elif numero < numero_aleatorio and intentos > 0:
+            print("Mas alto")
+            intentos -= 1
